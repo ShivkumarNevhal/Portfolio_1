0417cd8a7103b815a309c4684ca81295028e66f5
Shivkumar Nevhal <mastershiv8551@gmail.com>
2026-03-15 13:00:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -158,4 +158,5 @@ def delete_message(id):
     return jsonify({"message":"Deleted"})
 # IMPORTANT: run the server
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
